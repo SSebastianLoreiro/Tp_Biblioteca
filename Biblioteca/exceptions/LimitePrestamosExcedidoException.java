58795646461373cd34457fb7941cd1f4b0638978
SSebastianLoreiro <sergioyun155@gmail.com>
2026-04-19 23:01:58 -0300

    primer commit

---
+++ Biblioteca/exceptions/LimitePrestamosExcedidoException.java
@@ -0,0 +1,8 @@
+package Biblioteca.exceptions;
+
+public class LimitePrestamosExcedidoException extends Exception {
+
+    public LimitePrestamosExcedidoException(String message) {
+        super(message);
+    }
+}
